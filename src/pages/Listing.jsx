2c0184bd158f76708238362854fe730be1555246
Amanda Malamis <sitkoama@gmail.com>
2023-08-22 12:51:02 -0400

    add swiper

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react"
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from "../components/Spinner"
 import shareIcon from '../assets/svg/shareIcon.svg'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 
 function Listing() {
@@ -35,6 +39,13 @@ function Listing() {
     }
 
     return <main>
+        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+            {listings.imgUrls.map((url, index) => (
+                <SwiperSlide key={index}>
+                    <div className="swiperSlideDiv" style={{ background: `url(${listing.imgUrls[index]}) center no-repeat`, backgroundSize: 'cover' }}></div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
         <div className="shareIconDiv" onClick={() => {
             navigator.clipboard.writeText(window.location.href)
             setShareLinkCopied(true)
